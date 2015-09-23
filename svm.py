3e7c604e214fb8e7111c54cdf14d4544bafefeb9
fcanay <fcanay@hotmail.com>
2015-09-23 18:31:06 -0300

    subo los ejemplos q tenia

---
+++ svm.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import urllib
+from __future__ import print_function
+
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+def main():
+    # Loading the Digits dataset
+    digits = datasets.load_digits()
+
+    # To apply an classifier on this data, we need to flatten the image, to
+    # turn the data in a (samples, feature) matrix:
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    y = digits.target
+
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
+    raw_data = urllib.urlopen(url)
+    dataset = np.loadtxt(raw_data, dtype="string", delimiter=",")
+    le = preprocessing.LabelEncoder()
+    le.fit(["vhigh","high","med","low","5more","more","small","big","2","3","4","unacc","acc","good","vgood"])
+
+    dataset = le.transform(dataset)
+    
+
+    # Split the dataset in two equal parts
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.5, random_state=0)
+
+    # Set the parameters by cross-validation
+    tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                         'C': [1, 10, 100, 1000]},
+                        {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+    scores = ['precision', 'recall']
+
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5,
+                           scoring='%s_weighted' % score)
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        for params, mean_score, scores in clf.grid_scores_:
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean_score, scores.std() * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import urllib
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import cross_validation
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+
+def main():
+
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
+    raw_data = urllib.urlopen(url)
+    dataset = np.loadtxt(raw_data, dtype="string", delimiter=",")
+    le = preprocessing.LabelEncoder()
+    le.fit(["vhigh","high","med","low","5more","more","small","big","2","3","4","unacc","acc","good","vgood"])
+
+    dataset = le.transform(dataset)
+    X = dataset[:,0:6]
+    Y = dataset[:,6]
+    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.3, random_state=0)
+    clf = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree').fit(X_train, Y_train)
+    
+    NeighborsCount = []
+    trainScores = []
+    testScores = []
+    for i in range(1,500):
+        clf = KNeighborsClassifier(n_neighbors=i, algorithm='ball_tree').fit(X_train,Y_train)
+        NeighborsCount.append(i)
+        trainScores.append(clf.score(X_train,Y_train))
+        testScores.append(clf.score(X_test,Y_test))
+
+    plt.figure(1)
+    plt.plot(NeighborsCount, trainScores)
+    plt.plot(NeighborsCount, testScores)
+    plt.title(r"0% ruido")
+
+    #noise_idx = np.random.random(Y_train.shape)
+    #Y_train_with_noise = Y_train.copy()
+    #Y_train_with_noise[noise_idx<0.3] = np.floor(Y_train_with_noise[noise_idx<0.3] - 1) * (-1)
+    plt.show()
+    return
+    NeighborsCount = []
+    trainScores = []
+    testScores = []
+    for i in range(1,500):
+        clf = KNeighborsClassifier  (n_neighbors=i, algorithm='ball_tree').fit(X_train,Y_train_with_noise)
+        NeighborsCount.append(i)
+        trainScores.append(clf.score(X_train,Y_train))
+        testScores.append(clf.score(X_test,Y_test))
+
+    plt.figure(2)
+    plt.plot(NeighborsCount, trainScores)
+    plt.plot(NeighborsCount, testScores)
+    plt.title(r"10 variables random")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
