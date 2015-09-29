1f9ed971469a8764f572cd8577ee82b5a2607d52
fcanay <fcanay@hotmail.com>
2015-09-29 15:00:36 -0300

    knn funcionando, falta elegir dataset ajustar parametros y poner lindos los graficos

--- knn.py
+++ knn.py
@@ -5,9 +5,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+import random as rn
 
 def main():
 
+    NeighborsCount = range(1,50,1)
+    random_start = 0
+    random_end = 10
+    
     url = "http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
     raw_data = urllib.urlopen(url)
     dataset = np.loadtxt(raw_data, dtype="string", delimiter=",")
@@ -18,47 +23,66 @@ def main():
     X = dataset[:,0:6]
     Y = dataset[:,6]
     X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.3, random_state=0)
-    clf = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree').fit(X_train, Y_train)
     
-    NeighborsCount = []
-    trainScores = []
-    testScores = []
 
-for weights in ['uniform', 'distance']:
-    for i in range(1,500):
-        clf = KNeighborsClassifier(n_neighbors=i, algorithm='ball_tree',weights=weights).fit(X_train,Y_train)
-        NeighborsCount.append(i)
-        trainScores.append(clf.score(X_train,Y_train))
-        testScores.append(clf.score(X_test,Y_test))
+    trainScores = {}
+    testScores = {}
+    plt.figure(1)
+    plt.title(r"0% ruido")
+    plt.ylabel("Score")
+    plt.xlabel("Número de vecinos")
+    for weights in ['uniform', 'distance']:
+        trainScores[weights] = []
+        testScores[weights] = []
+        for i in NeighborsCount:
+            clf = KNeighborsClassifier(n_neighbors=i, algorithm='ball_tree',weights=weights).fit(X_train,Y_train)
+            trainScores[weights].append(clf.score(X_train,Y_train))
+            testScores[weights].append(clf.score(X_test,Y_test))
 
     #Plotear lo q corresponda
-    plt.figure(1)
-    plt.plot(NeighborsCount, trainScores)
-    plt.plot(NeighborsCount, testScores)
-    plt.title(r"0% ruido")
+        plt.plot(NeighborsCount, trainScores[weights],label='Train Score '+weights)
+        plt.plot(NeighborsCount, testScores[weights],label='Test Score '+weights)
 
     #noise_idx = np.random.random(Y_train.shape)
     #Y_train_with_noise = Y_train.copy()
     #Y_train_with_noise[noise_idx<0.3] = np.floor(Y_train_with_noise[noise_idx<0.3] - 1) * (-1)
+    plt.legend()
     plt.show()
-    return
-    NeighborsCount = []
-    trainScores = []
-    testScores = []
-    for cant_noise_var in [2,5,10,20]
+
+    plt.figure(2)
+    plt.title("0 ruido")
+    plt.ylabel("Score")
+    plt.xlabel("Numero de vecinos")
+
+    
+    X_train = X_train.tolist()
+    X_test = X_test.tolist()
+    add_vars = 0
+    for cant_noise_var in [0,20]:
+        add_vars += cant_noise_var
         #Agregar Variables random
-        for i in range(1,500):
-            clf = KNeighborsClassifier  (n_neighbors=i, algorithm='ball_tree').fit(X_train,Y_train_with_noise)
-            NeighborsCount.append(i)
-            trainScores.append(clf.score(X_train,Y_train))
-            testScores.append(clf.score(X_test,Y_test))
+        for i in xrange(0,cant_noise_var):
+            for j in xrange(0,len(X_train)):
+                X_train[j].append(rn.randint(random_start,random_end))
+            for j in xrange(0,len(X_test)):
+                X_test[j].append(rn.randint(random_start,random_end)) 
+        trainScores = {}
+        testScores = {}
+        for weights in ['uniform', 'distance']:
+            trainScores[weights] = []
+            testScores[weights] = []
+            for i in NeighborsCount:
+
+                clf = KNeighborsClassifier(n_neighbors=i, algorithm='ball_tree').fit(X_train,Y_train)
+                trainScores[weights].append(clf.score(X_train,Y_train))
+                testScores[weights].append(clf.score(X_test,Y_test))
+            plt.plot(NeighborsCount, trainScores[weights],label='Train Score '+weights+' '+str(add_vars))
+            plt.plot(NeighborsCount, testScores[weights],label='Test Score '+weights+' '+str(add_vars))
 
         #Plotear lo q corresponda
-        plt.figure(2)
-        plt.plot(NeighborsCount, trainScores)
-        plt.plot(NeighborsCount, testScores)
-        plt.title(r"10 variables random")
-        plt.show()
+    plt.legend()
+    
+    plt.show()
 
 if __name__ == '__main__':
     main()
