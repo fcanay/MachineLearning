4e5ae734a489a9f7ff6700261aecd8c53760ce2c
enano <enanodr@gmail.com>
2015-10-06 11:53:05 -0300

    idt con y sin ruido. Falta corregir el agregado de ruido

--- src/idt.py
+++ src/idt.py
@@ -1,3 +1,4 @@
+import urllib
 from sklearn import tree
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,32 +7,69 @@ from sklearn.datasets import load_iris
 
 
 def main():
-    iris = load_iris()
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+    raw_data = urllib.urlopen(url)
+    dataset = np.loadtxt(raw_data, dtype="string", delimiter=",")
+    
+    X = dataset[:,0:8]
+    Y = dataset[:,8]
     X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(iris.data, iris.target, test_size=0.2, random_state=0)
 
-    max_nodes = range(10,500,10)
+    random_seeds = range(0,100,5)
+    for seed in random_seeds:
+        
+        X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.3,random_state=seed)
 
-    trainScores = []
-    testScores = []
-    for i in max_nodes:
-        clf = tree.DecisionTreeClassifier(max_leaf_nodes=i).fit(X_train, Y_train)
-        trainScores.append(clf.score(X_train,Y_train))
-        testScores.append(clf.score(X_test,Y_test))
+        max_nodes = range(2,150,1)
 
-    plt.figure(1)
-    plt.title("Sobreajuste en IDT")
-    plt.ylabel("Score")
-    plt.xlabel("Numero de nodos maximos")
-    plt.plot(max_nodes, trainScores,label='Train Score')
-    plt.plot(max_nodes, testScores,label='Test Score') 
+        trainScores = []
+        testScores = []
+        for i in max_nodes:
+            clf = tree.DecisionTreeClassifier(max_leaf_nodes=i).fit(X_train, Y_train)
+            trainScores.append(clf.score(X_train,Y_train))
+            testScores.append(clf.score(X_test,Y_test))
 
-    plt.legend()
-    plt.show()  
+        plt.figure(2 * seed)
+        plt.title("Sobreajuste en IDT")
+        plt.ylabel("Score")
+        plt.xlabel("Numero de nodos maximos")
+        plt.plot(max_nodes, trainScores,label='Train Score')
+        plt.plot(max_nodes, testScores,label='Test Score') 
 
+        plt.legend()
+        file_path = "../informe/idt/noiseless/IDTwithSeed" + str(seed)
+
+        plt.savefig(file_path, dpi=None, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1,frameon=None)
     #noise_idx = np.random.random(Y_train.shape)
     #Y_train_with_noise = Y_train.copy()
     #Y_train_with_noise[noise_idx<0.3] = 1 - Y_train_with_noise[noise_idx<0.3]
 
+    
+        noise_percentages = [0.0,0.1,0.2,0.3,0.4,0.5]
+        noise_idx = np.random.random(Y_train.shape)
+        
+        for noise in noise_percentages:
+            y_train_with_noise = Y_train.copy()
+            #HACER ANDAR ESTA LINEA Y SALE.
+            #y_train_with_noise[noise_idx<noise] = np.floor(y_train_with_noise[noise_idx<nois] - 1)#y_train_with_noise[noise_idx<noise] - 1
+            trainScoresWithNoise = []
+            testScoresWithNoise = []
+            for i in max_nodes:
+                clf = tree.DecisionTreeClassifier(max_leaf_nodes=i).fit(X_train, Y_train)
+                trainScoresWithNoise.append(clf.score(X_train,Y_train))
+                testScoresWithNoise.append(clf.score(X_test,Y_test))
+
+            plt.figure(2 * seed + 1)
+            plt.title("Sobreajuste en IDT con ruido en ") 
+            plt.ylabel("Score")
+            plt.xlabel("Numero de nodos maximos")
+            plt.plot(max_nodes, trainScoresWithNoise,label='Train Score')
+            plt.plot(max_nodes, testScoresWithNoise,label='Test Score') 
+
+            plt.legend()
+            file_path = "../informe/idt/noiseless/IDTwithSeed" + str(seed)+"noise"+ str(noise)
+
+            plt.savefig(file_path, dpi=None, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1,frameon=None)
 
 
 
