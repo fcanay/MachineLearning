1564557a9edfcaf549bd874a3f29be4b729ff325
enano <enanodr@gmail.com>
2015-10-06 11:39:49 -0300

    knn grafica con un parametro de weights

--- src/knn.py
+++ src/knn.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 import numpy as np
 import urllib
 from sklearn.neighbors import KNeighborsClassifier
@@ -7,7 +8,7 @@ from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import random as rn
 
-def main():
+def main(weights):
 
     NeighborsCount = range(1,600,20)
     random_start = 0
@@ -16,24 +17,20 @@ def main():
     url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
     raw_data = urllib.urlopen(url)
     dataset = np.loadtxt(raw_data, dtype="string", delimiter=",")
-    #le = preprocessing.LabelEncoder()
-    #le.fit(["vhigh","high","med","low","5more","more","small","big","2","3","4","unacc","acc","good","vgood"])
-
-    #dataset = le.transform(dataset)
-
-    X = dataset[:,0:8]
-    #X = preprocessing.normalize(Z,copy=True)
-    Y = dataset[:,8]
-    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.2 )#,random_state=0)
     
-
-    trainScores = {}
-    testScores = {}
-    plt.figure(1)
-    plt.title(r"0% ruido")
-    plt.ylabel("Score")
-    plt.xlabel("Numero de vecinos")
-    for weights in ['distance']:#,'uniform']:
+    X = dataset[:,0:8]
+    Y = dataset[:,8]
+    
+    random_seeds = range(0,100,5)
+    for seed in random_seeds:
+        X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.2 ,random_state=seed)
+        trainScores = {}
+        testScores = {}
+        plt.figure(1)
+        plt.title(r"0% ruido")
+        plt.ylabel("Score")
+        plt.xlabel("Numero de vecinos")
+##      for weights in ['distance','uniform']:
         trainScores[weights] = []
         testScores[weights] = []
         for i in NeighborsCount:
@@ -45,32 +42,31 @@ def main():
         plt.plot(NeighborsCount, trainScores[weights],label='Train Score '+weights)
         plt.plot(NeighborsCount, testScores[weights],label='Test Score '+weights)
 
-    #noise_idx = np.random.random(Y_train.shape)
-    #Y_train_with_noise = Y_train.copy()
-    #Y_train_with_noise[noise_idx<0.3] = 1 - Y_train_with_noise[noise_idx<0.3]
-    plt.legend()
-    plt.show()
+        plt.legend()
 
-    plt.figure(2)
-    plt.title("0 ruido")
-    plt.ylabel("Score")
-    plt.xlabel("Numero de vecinos")
+        file_path = "../informe/knn/OriginalSample/KnnwithSeed" + str(seed) +"andWeight" + weights
+        plt.savefig(file_path, dpi=None, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1,frameon=None)
 
-    
-    X_train = X_train.tolist()
-    X_test = X_test.tolist()
-    add_vars = 0
-    for cant_noise_var in [0,20]:
-        add_vars += cant_noise_var
-        #Agregar Variables random
-        for i in xrange(0,cant_noise_var):
-            for j in xrange(0,len(X_train)):
-                X_train[j].append(rn.randint(random_start,random_end))
-            for j in xrange(0,len(X_test)):
-                X_test[j].append(rn.randint(random_start,random_end)) 
-        trainScores = {}
-        testScores = {}
-        for weights in ['uniform']:#,'uniform']:
+        plt.figure(2*seed + 1)
+        plt.title("0 ruido")
+        plt.ylabel("Score")
+        plt.xlabel("Numero de vecinos")
+
+        
+        X_train = X_train.tolist()
+        X_test = X_test.tolist()
+        add_vars = 0
+        for cant_noise_var in [0,20]:
+            add_vars += cant_noise_var
+            #Agregar Variables random
+            for i in xrange(0,cant_noise_var):
+                for j in xrange(0,len(X_train)):
+                    X_train[j].append(rn.randint(random_start,random_end))
+                for j in xrange(0,len(X_test)):
+                    X_test[j].append(rn.randint(random_start,random_end)) 
+            trainScores = {}
+            testScores = {}
+##          for weights in ['distance','uniform']:
             trainScores[weights] = []
             testScores[weights] = []
             for i in NeighborsCount:
@@ -81,10 +77,14 @@ def main():
             plt.plot(NeighborsCount, trainScores[weights],label='Train Score '+weights+' '+str(add_vars))
             plt.plot(NeighborsCount, testScores[weights],label='Test Score '+weights+' '+str(add_vars))
 
-        #Plotear lo q corresponda
-    plt.legend()
-    
-    plt.show()
+            #Plotear lo q corresponda
+        plt.legend()
+        
+        file_path = "../informe/knn/WithRandomVariables/KnnwithSeed" + str(seed) +"andWeight" + weights
+        plt.savefig(file_path, dpi=None, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1,frameon=None)
 
 if __name__ == '__main__':
-    main()
+    if (len(sys.argv) < 2):
+        print 'Usage: python knn.py [weights]'
+    else:
+        main(sys.argv[1])
