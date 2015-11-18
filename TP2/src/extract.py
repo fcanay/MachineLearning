b7ef1b23a25c2f656c2bdc4013a10677c204eaa8
fcanay <fcanay@hotmail.com>
2015-11-18 00:17:22 -0300

    corrijo extraccion

--- TP2/src/extract.py
+++ TP2/src/extract.py
@@ -19,13 +19,17 @@ def main(path,filename):
 
 def attributes_from(images):
     all_attributes = []
+    i = 1
     for image in images:
+        print "extrayendo atributos imagen " + i
+        i += 1
         all_attributes.append(extract_attributes(image))
     return all_attributes
 
 def extract_attributes(image):
-    print "extrayendo atributo"
+
     X = histogramaColor(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     X1 = np.concatenate((X,histogramaByN(image)))
     X2 = np.concatenate((X,Patrones3x3ByN(image)))
     return np.concatenate((X2,Patrones2x2ByN(image)))
@@ -41,15 +45,15 @@ def histogramaColor(image):
     return np.concatenate((b_hist,g_hist,r_hist))
 
 def histogramaByN(image):
-    greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
     hist,bins = np.histogram(greyImage.flatten(),256,[0,256])
     return hist
 
 def Patrones2x2ByN(image):
-    greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     patterns_dict = {'0000':0,'0001':0,'0010':0,'0100':0,'1000':0,'0011':0,'0101':0,'0110':0,'0111':0,'1001':0,'1010':0,'1011':0,'1100':0,'1101':0,'1110':0,'1111':0,}
-    for i in range(greyImage.shape[0]-1):
-        for j in range(greyImage.shape[1]-1):
+    for i in xrange(greyImage.shape[0]-1):
+        for j in xrange(greyImage.shape[1]-1):
             pattern = str(oscuro(greyImage[i,j])) + str(oscuro(greyImage[i+1,j]))+ str(oscuro(greyImage[i,j+1]))+ str(oscuro(greyImage[i+1,j+1]))
             if pattern in patterns_dict:
                 patterns_dict[pattern] += 1
@@ -59,15 +63,24 @@ def Patrones2x2ByN(image):
     return patterns_dict.values()
 
 def Patrones3x3ByN(image):
-    greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    patterns_dict = {'0000':0,'0001':0,'0010':0,'0100':0,'1000':0,'0011':0,'0101':0,'0110':0,'0111':0,'1001':0,'1010':0,'1011':0,'1100':0,'1101':0,'1110':0,'1111':0,}
+    #greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     patterns_dict = [0] * 512
-    for i in range(greyImage.shape[0]-2):
-        for j in range(greyImage.shape[1]-2):
-            pattern = oscuro(greyImage[i,j] + oscuro(greyImage[i+1,j] * 2 + oscuro(greyImage[i+2,j]) * 4 + oscuro(greyImage[i,j+1]) * 8 + oscuro(greyImage[i+1,j+1] * 16 + oscuro(greyImage[i+2,j+1]) * 32 + oscuro(greyImage[i,j+2]) * 64 + oscuro(greyImage[i+1,j+2] * 128 + oscuro(greyImage[i+2,j+2]) * 256
+    for i in xrange(greyImage.shape[0]-2):
+        for j in xrange(greyImage.shape[1]-2):
+            pattern = 0
+            for x in xrange(0,3):
+                for y in xrange(0,3):
+                    pattern += oscuro(greyImage[i+x,j+y]) * (2**(y+(3*x)))
+            #pattern = oscuro(greyImage[i,j]) + oscuro(greyImage[i+1,j]) * 2 + oscuro(greyImage[i+2,j]) * 4 + oscuro(greyImage[i,j+1]) * 8 + oscuro(greyImage[i+1,j+1] * 16 + oscuro(greyImage[i+2,j+1]) * 32 + oscuro(greyImage[i,j+2]) * 64 + oscuro(greyImage[i+1,j+2] * 128 + oscuro(greyImage[i+2,j+2]) * 256
             patterns_dict[pattern] += 1
 
     return patterns_dict
+
+def surf_extraccion(image,number_of_points,hessian=400):
+    surf = cv2.SURF(hessian)
+    surf.upright = True
+    kp= surf.detect(img,None)
+    #kp, des= surf.detectAndCompute(img,None)
 
 def oscuro(gris):
     if gris > 127:
@@ -98,6 +111,7 @@ def get_clases_from(imageFileNames):
         if fileName.find("cat") > -1 :
             clases.append("cat")
     return clases
+
 
 print 'hello'
 if len(sys.argv) != 2 and False:
